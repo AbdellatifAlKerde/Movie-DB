2cab8677926509d769519c01fe34b7950a07d36b
Abdellatif Al Kerde <boudikerde0@gmail.com>
2022-12-25 14:08:32 +0200

    step 3

--- index.js
+++ index.js
@@ -5,3 +5,19 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log("Ok"));
+
+// (/test)
+
+app.get("/test", (req, res, next) => {
+  res.json({ status: 200, message: "ok" });
+});
+
+// (/time)
+
+app.get("/time", (req, res, next) => {
+  let currentDate = new Date();
+  res.json({
+    status: 200,
+    message: `${currentDate.getHours()}:${currentDate.getSeconds()}`,
+  });
+});
